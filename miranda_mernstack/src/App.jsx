a9d7c39fe544deb33adbd0384760efa79652abe3
Errol Miranda <errol.miranda@cvsu.edu.ph>
2026-08-24 08:23:49 +0800

    Reusable Component

--- miranda_mernstack/src/App.jsx
+++ miranda_mernstack/src/App.jsx
@@ -8,7 +8,7 @@ function App (){
 <Student name= "Ranehart" age={20} section={3-1} student_no={202404078} course="BSIT"/><hr></hr>
 <Student name= "Buboy" age={20} section={3-1} student_no={202400152} course="BSIT"/><hr></hr>
 <Student name= "Keybin" age={20} section={3-1} student_no={202404070} course="BSIT"/><hr></hr>
-<Student name= "Jerawr" age={20} section={3-1} student_no={202404080} course="BSIT"/><hr></hr>
+<Student name= "Jerawrr" age={20} section={3-1} student_no={202404080} course="BSIT"/><hr></hr>
 
 
 </div>
