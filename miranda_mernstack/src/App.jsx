c702ddb7de37f8b5ba1917b49c09b96bce41f902
Errol Miranda <errol.miranda@cvsu.edu.ph>
2026-08-24 08:21:32 +0800

    first commit

---
+++ miranda_mernstack/src/App.jsx
@@ -0,0 +1,19 @@
+import Student from "./components/StudentComponent.jsx"
+function App (){
+
+
+  return(
+<div>
+<Student name= "Errol" age={20} section={3-1} student_no={202400153} course="BSIT"/><hr></hr>
+<Student name= "Ranehart" age={20} section={3-1} student_no={202404078} course="BSIT"/><hr></hr>
+<Student name= "Buboy" age={20} section={3-1} student_no={202400152} course="BSIT"/><hr></hr>
+<Student name= "Keybin" age={20} section={3-1} student_no={202404070} course="BSIT"/><hr></hr>
+<Student name= "Jerawr" age={20} section={3-1} student_no={202404080} course="BSIT"/><hr></hr>
+
+
+</div>
+  )
+}
+
+
+export default App;
---
+++ miranda_mernstack/src/components/StudentComponent.jsx
@@ -0,0 +1,19 @@
+
+function StudentComponent({name,age,section,student_no,course}){
+ 
+ return(
+<div>
+<p><span>Name: {name}</span></p>
+<p><span>Age:  {age}</span></p>
+<p><span>Section {section}</span></p>
+<p><span>Student_no. {student_no}</span></p>
+<p><span>Course {course}</span></p>
+
+
+
+
+
+</div>
+  )
+}
+export default StudentComponent;
